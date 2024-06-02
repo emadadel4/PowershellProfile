d9b83207add594eb8f3da7949e6348c788b36648
Emad Adel <emadadel1920@gmail.com>
2024-06-03 00:08:03 +0300

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -25,8 +25,7 @@ Profile-Links [github] [itt] [telegram]
 
 function Get-IP {
     ipconfig | findstr "IPv4"
-
-   
+}
 
 # Function to quickly navigate to commonly used directories
 function Go-To {
@@ -70,4 +69,14 @@ function InstallChoco {
 function Q {
 
     Clear-Host
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
