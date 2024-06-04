8260057679621558925d8f2bdd4fd38ef576ed75
Emad Adel <emadadel1920@gmail.com>
2024-06-04 06:38:56 +0300

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,82 +1,74 @@
 & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\json.omp.json" --print) -join "`n"))
-Import-Module -Name Terminal-Icons
-Import-Module PSReadLine
-Set-Alias ll Get-ChildItem
-Set-Alias cls Clear-Host
 
-Write-Host "
-Welcome 
-______ __  __    _    ____       _    ____  _____ _     
-| ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |    
-|  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |    
-| |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___ 
-|_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|
+    if (-not (Get-Module -Name Terminal-Icons -ListAvailable)) {
+        Install-Module -Name Terminal-Icons -Scope CurrentUser
+    }
+    else
+    {
+        Import-Module -Name Terminal-Icons
+    }
 
-https://github.com/emadadel4
-https://t.me/emadadel4
+    Import-Module PSReadLine
 
-Prompt to use
+    Set-Alias ll Get-ChildItem
+    Set-Alias cls Clear-Host
 
-Go-To [dev] [desktop]
-Profile-Links [github] [itt] [telegram]
+    Write-Host "
 
+    Welcome 
+    ______ __  __    _    ____          
+    | ____|  \/  |  / \  |  _ \    
+    |  _| | |\/| | / _ \ | | | |   
+    | |___| |  | |/ ___ \| |_| |  
+    |_____|_|  |_/_/   \_\____/  
 
 " -ForegroundColor Yellow
 
-function Get-IP {
-    ipconfig | findstr "IPv4"
-}
-
-# Function to quickly navigate to commonly used directories
-function Go-To {
+function emad {
+    [CmdletBinding()]
     param (
-        [string]$dir
+        [string]$Go,
+        [string]$open,
+        [switch]$Help,
+        [string]$Options
     )
-    switch ($dir) {
-        "dev" { Set-Location "C:\Users\$env:USERNAME\Documents\Github\ITT" }
-        "desktop" { Set-Location "C:\Users\$env:USERNAME\Desktop" }
-        default { Write-Host "Directory alias not found" }
+
+    if ($Help) {
+        Write-Host "usage: emad  [<command>] [<options>]  `n` "
+        Write-Host "The following commands are available:"
+        Write-Host "  -open         Specifies where to navigate. Available options: 'github' or 'itt' or 'telegram'."
+        Write-Host "  -go           Specifies where to navigate. Available options: 'desktop' or 'itt repo'."
+        Write-Host "  -Help         Display this help message."
+        return
     }
-}
 
-function Profile-Links {
-    param (
-        [string]$dir
-    )
-    switch ($dir) {
-        "github" { Start-Process ("https://github.com/emadadel4") }
-        "itt" { Start-Process ("https://github.com/emadadel4/ITT") }
-        "telegram" { Start-Process ("https://t.me/emadadel4") }
-
-        "help" { 
-
-            Write-Host "[github] = open Emad Adel Github profile"     
-            Write-Host "[itt] = open ITT Github Link"       
-            Write-Host "[telegram] = open Emad Adel telegram profile"       
-
+    switch ($open) 
+    {
+        "github" { 
+            Start-Process ("https://github.com/emadadel4") 
         }
 
-        default { Write-Host "there no command like that" }
+        "itt" { 
+            Start-Process ("https://github.com/emadadel4/itt") 
+        }
+        "telegram" { 
+            Start-Process ("https://t.me/emadadel4") 
+        }
+
     }
+
+    switch ($go) 
+    {
+        "desktop" { 
+            Set-Location "C:\Users\$env:USERNAME\Desktop"
+        }
+
+        "itt" { 
+            Set-Location "C:\Users\$env:USERNAME\Documents\Github\ITT" 
+        }
+    }
+
+  
 }
 
-function InstallChoco {
 
-    Write-Host "Installing Choco..."
-}
-
-
-function Q {
-
-    Clear-Host
-}
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
