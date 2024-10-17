15181b3fc78134b431985cc6598d9e963873c48a
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-17 04:59:55 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -36,6 +36,45 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue))
 
 " -ForegroundColor Yellow
 
+
+function Add-Log {
+
+    param (
+        [string]$Message, # Content of Message
+        [string]$Level = "INFO" # Message Level [INFO] [ERROR] [WARNING],
+    )
+
+    $timestamp = Get-Date -Format "hh:mm tt"
+
+    # Determine the color based on the log level
+    switch ($Level.ToUpper()) {
+        "INFO" { $color = "White" }
+        "WARNING" { $color = "Yellow" }
+        "ERROR" { $color = "Red" }
+        "Installed" { $color = "White" }
+        "Apply" { $color = "White" }
+        default { $color = "White" }
+    }
+
+    switch ($Level.ToUpper()) {
+        "INFO" { $icon = "!" }
+        "WARNING" { $icon = "!" }
+        "ERROR" { $icon = "X" }
+        "Installed" { $icon = "√" }
+        "Apply" { $icon = "√" }
+        "Disabled" { $icon = "X" }
+        "Enabled" { $icon = "√" }
+        default { $icon = "!" }
+    }
+
+    # Construct the log message
+    $logMessage =  "[$icon] $Message"
+
+    # Write the log message to the console with the specified color
+    Write-Host " $logMessage" -ForegroundColor $color
+
+}
+
 # Show help
 function help {
 
@@ -201,7 +240,7 @@ function ch {
     
     if (Test-Path $historyFilePath) {
         Remove-Item $historyFilePath -Force
-        Write-Output "Commands history cleard!"
+        Add-Log -Message "History Claerd" -Level "Info"
     } else {
         Write-Output "ConsoleHost_history.txt not found."
     }
