6838098b142f339875491a54bea30247f943e18a
Emad Adel <emadadel1920@gmail.com>
2025-02-15 08:07:54 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,60 +9,7 @@ else {
 Import-Module PSReadLine
 Set-Alias ll Get-ChildItem
 Set-Alias cls Clear-Host
-Write-Host "
-        ⠈⠙⠲⢶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⡀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⣼⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⠟⠓⠉
-        ⠀⠀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⢀⣧⣶⣦⣇⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣾⣿⣿⣿⣿⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠛⠛⠛⠛⠛⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠟⠛⠛⠛⠛⠛⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠐⠿⣿⡿⢿⣶⣿⠂⢀⣾⣿⠃⢀⣴⣿⡟⠁⠀⠀⣰⣿⢟⣿⣿⠟⠀⠀⣠⣾⣿⡇⣿⣿⣿⣿⣿⠀⣶⣿⣿⠂⣠⣾⣿⡟⠀⢀⣴⣿⣿⠂⢀⣾⣿⡀⢠⣿⡇⠀⣼⣿⡿
-        ⠀⠀⠀⣴⡟⠀⠀⠉⠁⢀⣾⢿⣿⣰⡿⣿⡿⠁⠀⠀⣰⣿⣿⢿⣿⣷⠀⣠⣾⣿⢋⣿⡟⠀⣼⡿⠁⠀⣰⣿⣿⣿⣾⣿⣿⣿⠀⣰⣿⡿⣻⣿⢀⣾⡿⣿⣧⣾⡟⠀⣰⣿⠏⠀
-        ⠀⣀⣼⣿⣤⠀⠀⠀⢠⣿⠇⢸⣿⠏⣸⣿⠇⠀⠀⢰⣿⣿⣤⣴⣿⠿⣴⣿⣿⠿⢿⣿⡇⣸⣿⠃⠀⢰⣿⠇⣿⣿⠏⣼⣿⢇⣾⣿⡿⠿⣿⡇⣼⣿⠃⢸⣿⡿⠀⣠⣿⡍⠀⠀
-        ⠙⠛⠉⠉⠁⠀⠀⠀⠀⠛⠀⠀⠁⠀⠈⠉⠀⠀⠀⠀⠙⠛⠛⠉⠀⠀⠀⠛⠃⠀⠀⠀⠘⠛⠀⠛⠃⠀⠀⠉⠋⠀⠙⠁⠀⠘⠋⠀⠘⠋⠀⠀⠀⠛⠀⠛⠃⠀⠀⠛⠀⠀⠈⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-" -ForegroundColor White
 
-function Add-Log {
-
-    param (
-        [string]$Message, # Content of Message
-        [string]$Level = "INFO" # Message Level [INFO] [ERROR] [WARNING],
-    )
-
-    $timestamp = Get-Date -Format "hh:mm tt"
-
-    # Determine the color based on the log level
-    switch ($Level.ToUpper()) {
-        "INFO" { $color = "White" }
-        "WARNING" { $color = "Yellow" }
-        "ERROR" { $color = "Red" }
-        "Installed" { $color = "White" }
-        "Apply" { $color = "White" }
-        default { $color = "White" }
-    }
-
-    switch ($Level.ToUpper()) {
-        "INFO" { $icon = "+" }
-        "WARNING" { $icon = "!" }
-        "ERROR" { $icon = "X" }
-        "Installed" { $icon = "√" }
-        "Apply" { $icon = "√" }
-        "Disabled" { $icon = "X" }
-        "Enabled" { $icon = "√" }
-        default { $icon = "!" }
-    }
-
-    # Construct the log message
-    $logMessage = "$Message"
-
-    # Write the log message to the console with the specified color
-    Write-Host "$logMessage" -ForegroundColor $color
-
-}
 # Show help
 function help {
 
